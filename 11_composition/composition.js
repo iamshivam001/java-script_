755382b6f00441a03aeb8b358e8bb6021b5d508e
iamshivam001 <165639103+iamshivam001@users.noreply.github.com>
2024-04-27 10:15:50 +0000

    compostion

---
+++ 11_composition/composition.js
@@ -0,0 +1,70 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function square(val) {
+//     return val * val ;
+// }
+
+// function multiply(a,b) {
+//     return a * b ;
+// }
+
+
+// const addResult = add(2,3);
+// console.log(square(addResult));
+
+
+// function addTwoandSquare (a,b){
+//     return square(add(a,b));
+// }
+// console.log(addTwoandSquare(2,3));
+
+
+// function composeTwoFunction(fn1,fn2) {
+//     return function (a, b ) {
+//         return fn2(fn1(a,b));
+//     };
+// }
+
+// const task = composeTwoFunction(multiply , square);
+// console.log(task(5,3));
+
+
+// change it in this ECHME6 or Modern javascript 
+
+
+
+// const c2f = (fn1,f2) => (a,b) => f2(fn1(a,b));
+
+// const task = c2f(multiply , square);
+// console.log(task(5,3));
+
+
+// how you can compose unlimited function 
+function add(a, b) {
+    return a + b;
+}
+
+function square(val) {
+    return val * val ;
+}
+
+function multiply(arg) {
+    return arg[0] * arg[1] ;
+}
+
+
+function compose(...fns) {
+    return function(...values) {
+      return  fns.reduceRight((a,b) => b(a), values);
+    }
+}
+
+const c2f = (fn1,f2) => (a,b) => f2(fn1(a,b));
+
+
+
+
+const task = c2f(multiply , square);
+console.log(task(5,3));
